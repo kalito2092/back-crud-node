b8ac0c8c6da9b5164450c6a3b80bfd33cc723746
kalito2092 <the_rasmus1869@hotmail.com>
2021-11-28 12:44:56 +0100

    First commit backend

---
+++ models/Producto.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ProductoSchema = new mongoose.Schema({
+    nombre : {
+        type: String,
+        required: true
+    },
+    categoria : {
+        type: String,
+        required: true
+    },
+    ubicacion : {
+        type: String,
+        required: true
+    },
+    precio : {
+        type: Number,
+        required: true
+    },
+    fechaCreacion : {
+        type: Date,
+        default: Date.now()
+    }
+
+  });
+
+  module.exports = mongoose.model('Producto', ProductoSchema);
